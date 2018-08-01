2e72cfe4515c9aae0b0293fb15f69727932c062f
Yun Phelps <malia.p.y@gmail.com>
2018-07-31 21:36:37 -0500

    Init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const path = require('path'); 
+
+const publicPath = path.join(__dirname, '../public');
+const port  = process.env.PORT || 3000;
+var app = express(); 
+
+app.use(express.static(publicPath)); 
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`); 
+});
+
+//make commit with current files
+//git hub create repo
+//push local code up
